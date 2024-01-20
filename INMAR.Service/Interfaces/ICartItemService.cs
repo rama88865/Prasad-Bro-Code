69ea6c5cb44096d8d8afde7cb8f18d42cd3482aa
Durga Prasad Baghuthu <dbaghuthu@betalen.in>
2024-01-20 07:15:30 +0530

    adding cartiem service

---
+++ INMAR.Service/Interfaces/ICartItemService.cs
@@ -0,0 +1,14 @@
+﻿using INMAR.Service.Models;
+
+namespace INMAR.Service.Interfaces
+{
+    public interface ICartItemService
+    {
+        Task<bool> AddOrUpdateCartItem(CartItem cartItem);
+        Task<bool> DeleteCartItem(CartItem cartItem);
+        Task<IEnumerable<CartItem>> GetCartItems();
+        Task<IEnumerable<CartItem>> GetCartItem(long cartItemId);
+        Task<IEnumerable<CartItem>> GetCartItemsByUserId(long userId);
+        Task<IEnumerable<CartItem>> GetCartItemsByUserId(long userId, long cartItemId);
+    }
+}
